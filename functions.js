daa3086ec66df763920065200ead05e109e0d9d3
C_ffeeStain <deaconavery2006@gmail.com>
2023-11-11 09:15:12 -0600

    refactor functions.js for readability

--- functions.js
+++ functions.js
@@ -17,39 +17,28 @@ function redirect(option) {
     }
 }
 
-function startTest(option) {
-    const num = document.getElementById("question");
-    switch (option) {
-        // binary test
-        case 1:
-            num.innerText = Math.floor(Math.random() * 255).toString();
-            break;
+const numOfBits = 8;
+const maxNum = 255;
 
-        default:
-            break;
-    }
-}
+let numQ = 0;
 
-function bytesNeeded(num) {
-    let exp = closestPowerOf2(num);
-    console.log(exp)
-    return Math.ceil(exp / 8);
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * max) + min;
 }
 
 function toggleBit(num) {
     let bit = document.getElementById("b" + num.toString());
     if (bit.innerText == "0") {
         bit.innerText = "1";
-    } else {
-        bit.innerText = "0";
+        return;
     }
+    bit.innerText = "0";
 }
 
-function checkBits() {
-    const num = document.getElementById("question");
-
+// Fetch the user's bits and put them into a list
+function getBits() {
     let bits = [];
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < numOfBits; i++) {
         let bit = document.getElementById(`b${i}`);
         if (bit) {
             bits.push(bit.innerText);
@@ -57,51 +46,52 @@ function checkBits() {
             alert("couldn't find bit #" + i.toString());
         }
     }
+    return bits;
+}
 
+// Convert list of bits into an integer
+function binToInt(bits) {
     let numValue = 0;
     for (let index = 0; index < bits.length; index++) {
         const bit = bits[index];
         if (bit == "1") {
-            numValue += 2 ** (7 - index);
+            numValue += 2 ** (numOfBits - 1 - index);
         }
     }
-    console.log(numValue);
-    if (numValue == Number.parseInt(num.innerText)) {
-        const check = document.getElementById("check");
-        
-        check.parentElement.removeChild(check);
-        
-        let prompt = document.getElementById("prompt");
-        prompt.innerText = "Correct! Reload the page to do it again!";
-        prompt.style.color = "rgb(93, 160, 79)";
+}
 
-        return;
-    }
-    let prompt = document.getElementById("prompt")
+// Tell user that answer is incorrect, revert to normal prompt after 1/2 of a second, called when number is incorrect
+function handleIncorrect(prompt) {
     let buffer = prompt.innerHTML;
-    prompt.innerHTML = "Incorrect!"
+
+    prompt.innerHTML = "Incorrect!";
     prompt.style.color = "rgb(247, 66, 59)";
+
     setTimeout(() => {
         prompt.innerHTML = buffer;
         prompt.style.color = "white";
     }, 500);
 }
 
-function closestPowerOf2(number) {
+// Remove the Check button and tell the user to reload, called when number is correct
+function handleCorrect(prompt) {
+    const check = document.getElementById("check");
 
-    // Find the closest power of 2 greater than the given number
-    let power = 1;
-    let exp = 1;
-    while (power < number) {
-        power *= 2;
-        exp++;
+    check.parentElement.removeChild(check);
+
+    prompt.innerText = "Correct! Reload the page to do it again!";
+    prompt.style.color = "rgb(93, 160, 79)";
+}
+
+// Check if user's bits are correct, handle accordingly
+function checkBits() {
+    let numValue = binToInt(getBits());
+
+    let prompt = document.getElementById("prompt")
+    
+    if (numValue == numQ) {
+        handleCorrect(prompt);
+        return;
     }
-
-    // Compare the distance of the two closest powers of 2
-    const prevPower = power / 2;
-    const prevExp = exp - 1;
-
-    const nextExp = exp;
-    const nextPower = power;
-    return (nextPower - number) < (number - prevPower) ? nextExp - 1 : prevExp - 1;
+    handleIncorrect(prompt)
 }
