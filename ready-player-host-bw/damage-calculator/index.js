aa2e6f26dae11a8369394449ad460908f1e1a886
C-ffeeStain <deaconavery2006@gmail.com>
2026-02-07 00:32:51 -0600

    added stuff for RPH

---
+++ ready-player-host-bw/damage-calculator/index.js
@@ -0,0 +1,57 @@
+let dmgAmount, atkAttack, atkElement, atkCritHit, defDefense, defElement, defDefended;
+
+addEventListener("load", (event) => {
+    dmgAmount = document.getElementById("dmg-amount");
+    atkAttack = document.getElementById("atk-damage");
+    atkElement = document.getElementById("atk-elements");
+    atkCritHit = document.getElementById("atk-crithit");
+    defDefense = document.getElementById("def-defense");
+    defElement = document.getElementById("def-elements");
+    defDefended = document.getElementById("def-defended");
+
+    document.getElementById("calculate").onclick = calculateDamage;
+});
+
+
+function calculateDamage() {
+    dmgAmount.innerHTML = "0";
+    let damage = parseFloat(atkAttack.value);
+    if (isNaN(damage)) return;
+    console.log(atkElement.value)
+    switch (atkElement.value) {
+        case "fire":
+            if (defElement.value == "water") damage /= 2;
+            else if (defElement.value == "ground") damage *= 2;
+            break;
+        case "water":
+            if (defElement.value == "electric") damage /= 2;
+            else if (defElement.value == "fire") damage *= 2;
+            break;
+        case "electric":
+            if (defElement.value == "ground") damage /= 2;
+            else if (defElement.value == "water") damage *= 2;
+            break;
+        case "ground":
+            if (defElement.value == "fire") damage /= 2;
+            else if (defElement.value == "electric") damage *= 2;
+            break;
+        case "light":
+            if (defElement.value == "dark") damage *= 2;
+            break;
+        case "dark":
+            if (defElement.value == "light") damage *= 2;
+            break;
+        default:
+            break;
+    }
+
+    if (atkCritHit.checked) damage *= 1.5;
+
+    if (defDefended.checked) {
+        damage *= (100 - 50 + Math.max(Math.min(parseInt(defDefense.value), 40), 2)) / 100;
+    }
+
+    damage -= parseInt(defDefense.value);
+    
+    dmgAmount.innerHTML = Math.floor(damage).toString();
+}
